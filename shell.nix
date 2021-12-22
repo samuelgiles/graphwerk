7161bb116fa9dc96a56b41672b9d953d8c0daaf5
Michael Webb <michaelwebb76@gmail.com>
2021-12-22 20:22:09 +1100

    Do not ignore shell.nix

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ sources ? import ./nix/sources.nix }:
+let
+  nixpkgs = import sources.nixpkgs { };
+in
+nixpkgs.mkShell {
+  name = "bellroy-gem-env";
+  buildInputs = with nixpkgs; [
+    bundler
+    libnotify
+    niv
+    pkg-config
+    readline
+    ruby_2_7
+    zlib
+  ]
+  ++ (if stdenv.hostPlatform.isDarwin then [ libiconv darwin.apple_sdk.frameworks.CoreServices ] else [ ]);
+  shellHook = ''
+    bundle config --local path "$PWD/vendor/bundle"
+  '';
+}
